21f240e10db266d400291c8ca5aa87fa8cbe1547
kiplrry <kiplarry19@gmail.com>
2024-05-18 14:43:29 +0300

    0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,6 +4,7 @@ Simple helper function
 """
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     takes two integer arguments page and page_size.
